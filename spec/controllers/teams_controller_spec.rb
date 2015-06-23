d807df1346c85659e933941b2654472e96e29a27
etaimiller <etaimills@gmail.com>
2015-06-23 17:28:29 +0200

    teams controller index and create

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -30,7 +30,7 @@ describe TeamsController do
     context "with invalid parameters" do
       let(:invalid_attrs) { { team: { users: [user1, user2, "bleh"].to_json } } }
 
-      it "should return 200 - Ok" do
+      it "should return 200 - Unprocessable entity" do
         post :create, invalid_attrs, format: :json
         expect(response).to have_http_status(:unprocessable_entity)
       end
@@ -47,12 +47,35 @@ describe TeamsController do
   end
 
   describe '#index' do
-    context "when teams exist" do
 
+    before { sign_in user1 }
+
+    context "when teams exist" do
+      let!(:team1) { create(:team) }
+      let!(:team2) { create(:team) }
+
+      it "should return 200 - Ok" do
+        get :index, format: :json
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "should return an array of teams" do
+        get :index, format: :json
+        expect(JSON.parse(response.body)['data'].to_json).to eq [team1,team2].to_json
+      end
     end
 
     context "when no teams exist" do
 
+      it "should return 204 - No content" do
+        get :index, format: :json
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it "should return empty array" do
+        get :index, format: :json
+        expect(JSON.parse(response.body)['data']).to be_nil
+      end
     end
   end
 end
